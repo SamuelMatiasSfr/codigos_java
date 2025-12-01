bee0d9b30f540bf82a8f4cdbc88945f353af93a8
Samuel Matias <136845273+SamuelMatiasSfr@users.noreply.github.com>
2025-12-01 18:47:48 -0300

    Delete LP2-Prova6 directory

--- LP2-Prova6/InterfaceAlunosDao.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public interface InterfaceAlunosDao {
-
-    public ArrayList<Aluno> getTodosAlunos(float notaMinima);
-    public void atualizarAluno(int id, float nota1, float nota2, float nota3);
-
-}
