e8b1746401b0e97685daf311818da388fb94b77b
Samuel Matias <136845273+SamuelMatiasSfr@users.noreply.github.com>
2026-01-14 11:42:28 -0300

    Delete LP2-RMI-TratamentoExcecao directory

--- LP2-RMI-TratamentoExcecao/UsuarioNaoAutorizadoException.java
+++
@@ -1,9 +0,0 @@
-//Projeto criado por IA
-
-public class UsuarioNaoAutorizadoException extends Exception {
-
-    public UsuarioNaoAutorizadoException(String nome) {
-        super("Usuário não autorizado: " + nome);
-    }
-
-}
